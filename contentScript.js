2dbf60be8d44beb350028b227286e34e8dc93898
Grace Wang <gracewang92@gmail.com>
2021-08-21 14:50:40 -0700

    filter out non-first-order connections & promoted posts

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,9 @@
 window.addEventListener("scroll", function () {
-  console.log(document.getElementsByClassName("feed-shared-text"));
+  const v = Array.prototype.slice.call(document.getElementsByClassName("feed-shared-update-v2"))
+    // filter out non-first-order activity
+    .filter(el => el.querySelector(".feed-shared-header") === null)
+    // filter out Promoted posts
+    .filter(el => el.querySelector(".feed-shared-actor__sub-description").innerText !== "Promoted")
+    .map(el => el.querySelector(".feed-shared-text span span").innerText);
+  console.log(v);
 });
