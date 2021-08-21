2853dd197e6ba70cee1327553918b660f7e9dbd5
Grace Wang <gracewang92@gmail.com>
2021-08-21 15:59:21 -0700

    display post text in chrome ext

---
+++ build.sh
@@ -0,0 +1,9 @@
+# https://dev.to/alexbh/using-next-js-to-create-a-browser-extension-chrome-firefox-490h
+
+yarn build
+
+cp manifest.json ./out
+cp contentScript.js ./out
+cp background.js ./out
+mv ./out/_next ./out/next
+cd ./out && grep -rli '_next' * | xargs -I@ sed -i '' 's/_next/next/g' @;
--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,16 @@
 window.addEventListener("scroll", function () {
-  const v = Array.prototype.slice.call(document.getElementsByClassName("feed-shared-update-v2"))
+  const postElements = Array.prototype.slice.call(document.getElementsByClassName("feed-shared-update-v2"))
     // filter out non-first-order activity
-    .filter(el => el.querySelector(".feed-shared-header") === null)
+    //.filter(el => el.querySelector(".feed-shared-header") === null)
     // filter out Promoted posts
-    .filter(el => el.querySelector(".feed-shared-actor__sub-description").innerText !== "Promoted")
-    .map(el => el.querySelector(".feed-shared-text span span").innerText);
-  console.log(v);
+    .filter(el => el.querySelector(".feed-shared-actor__sub-description")?.innerText !== "Promoted");
+
+  const data = Object.assign({}, ...postElements.map((el) => ({ [el.id]: {
+    name: el.querySelector(".feed-shared-actor__name span")?.innerText,
+    imageSrc: el.querySelector(".feed-shared-actor__avatar-image").src,
+    text: el.querySelector(".feed-shared-text span span")?.innerText,
+  }})));
+
+  console.log(data);
+  chrome.runtime.sendMessage(data);
 });
